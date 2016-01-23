c268ce0124b8511b23fc3121e5765f327b2cd31a
JASMINEIRIS <JASMINEIRISDIAZ@GMAIL.COM>
2016-01-22 23:31:37 -0800

    updated

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -52,6 +52,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let pictures = pictures {
@@ -60,6 +61,16 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             return 0
         }
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let pictures = pictures {
+            return pictures.count
+        } else {
+            return 0
+        }
+    }
+    
+
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
@@ -73,6 +84,27 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+        
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).CGColor
+        profileView.layer.borderWidth = 1;
+        
+        // Use the section number to get the right URL
+        // profileView.setImageWithURL(...)
+        
+        headerView.addSubview(profileView)
+        
+        // Add a UILabel for the username here
+        
+        return headerView
+    }
 
 }
 
